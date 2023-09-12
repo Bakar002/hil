06094b17ae60d2e6a22d7fd8e9000fb6fd3d437b
Bakar002 <abubakaramjad78600@gmail.com>
2023-09-13 03:47:44 +0500

    updated

--- src/components/Animation/Gallery.js
+++ src/components/Animation/Gallery.js
@@ -1,94 +1,89 @@
 import React, { useState, useEffect, useRef } from 'react';
-import './style.css'; // Include your CSS file for styling
+import './style.css';
+
 const cardsData = [
-    {
-      title: 'Card 1',
-      imageUrl: '/assets/3.webp',
-    },
-    {
-      title: 'Card 2',
-      imageUrl: '/assets/1.webp',
-      
-    },
-    {
-      title: 'Card 3',
-      imageUrl: '/assets/2.webp',
-      
-    },
-  ];
-  
-  const Gallery = () => {
-    const [scrollLock, setScrollLock] = useState(true);
-    const [activeCardIndex, setActiveCardIndex] = useState(1); // Initialize the active card index to 1
-    const cardContainerRef = useRef(null);
-  
-    const scrollThreshold = 20; // Adjust this value for scroll sensitivity
-    const scrollDelay = 3000; // Adjust this value for the delay between index changes (in milliseconds)
-    let isChangingIndex = false;
-  
-    useEffect(() => {
-      const cardContainer = cardContainerRef.current;
-  
-      const handleScroll = (e) => {
-        // Prevent default scroll behavior when the overflow is hidden
-        if (scrollLock) {
-          e.preventDefault();
-        }
-  
-        const deltaY = e.deltaY;
-  
-        if (!isChangingIndex) {
-          if (deltaY > scrollThreshold) {
-            // Check if the card container is in the viewport before changing the index
-            if (isElementInViewport(cardContainer) && activeCardIndex < 2) {
-              isChangingIndex = true;
+  {
+    title: 'Card 1',
+    imageUrl: '/assets/3.webp',
+  },
+  {
+    title: 'Card 2',
+    imageUrl: '/assets/1.webp',
+  },
+  {
+    title: 'Card 3',
+    imageUrl: '/assets/2.webp',
+  },
+];
+
+const Gallery = () => {
+  const [scrollLock, setScrollLock] = useState(true);
+  const [activeCardIndex, setActiveCardIndex] = useState(1);
+  const cardContainerRef = useRef(null);
+
+  const scrollThreshold = 20; // Adjust this value for scroll sensitivity
+  const scrollDelay = 200; // Adjust this value for the delay between index changes (in milliseconds)
+  let isChangingIndex = false;
+
+  useEffect(() => {
+    const cardContainer = cardContainerRef.current;
+
+    const handleScroll = (e) => {
+      if (scrollLock) {
+        e.preventDefault();
+      }
+
+      const deltaY = e.deltaY;
+
+      if (!isChangingIndex) {
+        if (deltaY > scrollThreshold) {
+          if (isElementInViewport(cardContainer) && activeCardIndex < 2) {
+            isChangingIndex = true;
+            
+            setTimeout(() => {
               setActiveCardIndex((prevIndex) => prevIndex + 1);
-              setTimeout(() => {
-                isChangingIndex = false;
-              }, scrollDelay);
-            }
-          } else if (deltaY < -scrollThreshold) {
-            if (isElementInViewport(cardContainer) && activeCardIndex > 0) {
-              isChangingIndex = true;
+              isChangingIndex = false;
+            }, scrollDelay);
+          }
+        } else if (deltaY < -scrollThreshold) {
+          if (isElementInViewport(cardContainer) && activeCardIndex > 0) {
+            isChangingIndex = true;
+          
+            setTimeout(() => {
               setActiveCardIndex((prevIndex) => prevIndex - 1);
-              setTimeout(() => {
-                isChangingIndex = false;
-              }, scrollDelay);
-            }
+              isChangingIndex = false;
+            }, scrollDelay);
           }
         }
-  
-        // Lock or unlock scroll based on the active card index
-        if (activeCardIndex === 2 && scrollLock) {
-          setScrollLock(false); // Lock scrolling when active card index is 2
-        } else if (activeCardIndex !== 2 && !scrollLock) {
-          setScrollLock(true); // Unlock scrolling when active card index is not 2
-        }
-      };
-  
-      window.addEventListener('wheel', handleScroll, { passive: false });
-  
-      return () => {
-        window.removeEventListener('wheel', handleScroll);
-      };
-    }, [scrollLock, activeCardIndex]);
-  
-    // Function to check if an element is in the viewport
-    const isElementInViewport = (element) => {
-      const rect = element.getBoundingClientRect();
-      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-      const viewportTop = window.scrollY;
-      const viewportBottom = viewportTop + windowHeight;
-  
-      return rect.bottom >= viewportTop && rect.top <= viewportBottom;
+      }
+
+      if (activeCardIndex === 2 && scrollLock) {
+        setScrollLock(false);
+      } else if (activeCardIndex !== 2 && !scrollLock) {
+        setScrollLock(true);
+      }
     };
-  
-    // Define a shuffled array of cards based on the activeCardIndex
-    const shuffledCardsData = [
-      ...cardsData.slice(activeCardIndex),
-      ...cardsData.slice(0, activeCardIndex),
-    ];
-  
+    window.addEventListener('wheel', handleScroll, { passive: false });
+
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+    };
+  }, [scrollLock, activeCardIndex]);
+
+  const isElementInViewport = (element) => {
+    const rect = element.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    const viewportTop = window.scrollY;
+    const viewportBottom = viewportTop + windowHeight;
+
+    return rect.bottom >= viewportTop && rect.top <= viewportBottom;
+  };
+
+  const shuffledCardsData = [
+    ...cardsData.slice(activeCardIndex),
+    ...cardsData.slice(0, activeCardIndex),
+  ];
+
     return (
       <div
         ref={cardContainerRef}
--- src/components/Cards/FlippingCard.js
+++ src/components/Cards/FlippingCard.js
@@ -2,44 +2,74 @@ import { useRef } from 'react';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
 import { Link } from 'react-router-dom';
 import "./FlippingCard.css"
+
 function FlippingCard({ title, detail, image, path }) {
   const ref = useRef();
+
   return (
-    <div className="">
-    <Flippy
-      flipOnHover={true}
-      flipOnClick={true}
-      flipDirection="horizontal"
-      ref={ref}
-      style={{  width: '300px', height: '400px', borderRadius: '10px', border:'none'}}
-    >
-      <FrontSide style={{  backgroundColor: 'transparent', height: '400px', boxShadow: 'none', border: 'none'}}>
-        <img src={image} alt={title} className="w-full h-full object-contain  " />
-       
-      </FrontSide>
-      <BackSide style={{ backgroundColor: 'white', borderRadius: '10px', height: 'auto', width: '250px', marginTop: '20px' }}>
-  <div className="text-black h-full flex flex-col justify-between p-2">
-    <div className="mb-1">
-      <h3 className="text-2xl font-semibold mx-auto">{title}</h3>
-      <p className="mt-2">{detail}</p>
-    </div>
-    <div className="flex justify-center mx-auto">
-      <Link to={path}>
-        <button type="button" className="btn btn-red" onClick={() => scrollToTop()}>
-          Get the product
-        </button>
-      </Link>
-    </div>
+    <div className=" flex flex-wrap">
+      <Flippy
+        flipOnHover={true}
+        flipOnClick={true}
+        flipDirection="horizontal"
+        ref={ref}
+        style={{
+          width: '100%', // Use the full width of the container
+          height: '70vh', // Set the height to 70vh
+          borderRadius: '10px',
+          border: 'none',
+          display: 'flex', // Use flex to center content vertically
+          flexDirection: 'column',
+          
+           // Stack the front and back sides vertically
+        }}
+      >
+        <FrontSide style={{
+          backgroundColor: 'transparent',
+          flex: 1, // Take up all available space vertically
+          boxShadow: 'none',
+          border: 'none',
+        }}>
+          <img
+            src={image}
+            alt={title}
+            style={{
+              width: '100%', // Make the image cover the entire front side
+              height: '100%', // Make the image cover the entire front side
+              objectFit: 'cover', // Preserve aspect ratio while covering the container
+            }}
+          />
+        </FrontSide>
+        <BackSide style={{
+  backgroundColor: 'white',
+  borderRadius: '4px',
+  flex: 1, // Take up all available space vertically
+  display: 'flex', // Use flex to center content vertically
+  flexDirection: 'column', // Stack content vertically
+  justifyContent: 'center', // Center content vertically
+  alignItems: 'center', // Center content horizontally
+  padding: '20px', // Add padding for spacing
+}}>
+  <div className="text-black text-center">
+    <h3 className="text-2xl font-semibold">{title}</h3>
+    <p className="mt-2">{detail}</p>
+  </div>
+  <div className="flex justify-center mt-3">
+    <Link to={path}>
+      <button type="button" className="btn btn-red" onClick={() => scrollToTop()}>
+        Get the product
+      </button>
+    </Link>
   </div>
 </BackSide>
 
-
-
-    </Flippy>
+      </Flippy>
     </div>
-  )
+  );
+
   function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
+
 export default FlippingCard;
