8ab4eca74c0c7a2ef15c62b9393d2853c0c49706
Bakar002 <abubakaramjad78600@gmail.com>
2023-09-21 14:17:17 +0500

    updated

--- src/components/Footer/SocialIcons.jsx
+++ src/components/Footer/SocialIcons.jsx
@@ -4,14 +4,15 @@ const SocialIcons = ({ Icons }) => {
   return (
     <div className="text-black">
       {Icons.map((icon) => (
-        <span
-          key={icon.name}
+        <a
+        key={icon.name}
+        href={icon.link} // Set the href attribute to the desired URL
           className="p-2 cursor-pointer inline-flex items-center
         rounded-full bg-[#EEEDED] mx-1.5 text-xl hover:text-gray-100 hover:bg-[#D71313]
         duration-300 "
         >
           <ion-icon name={icon.name}></ion-icon>
-        </span>
+        </a>
       ))}
     </div>
   );
